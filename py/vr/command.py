3d5679c09429f3d1a27c56ec223a6910a389d483
Eugene Skepner <github@skepner.eu>
2020-06-18 16:38:17 +0200

    vr: get_merges

--- py/vr/command.py
+++ py/vr/command.py
@@ -1,13 +1,17 @@
 import sys
 import logging; module_logger = logging.getLogger(__name__)
+from pathlib import Path
 from . import report, map
 from .error import Error
 
 # ----------------------------------------------------------------------
 
+def __get_merges(command): get_merges()
+
 sCommands = {
     "report": report.make_report,
     "report-addendum": report.make_addendum,
+    "~get-merges": __get_merges,
     }
 
 from report import maps
@@ -21,6 +25,14 @@ def process(command):
     if not cmd:
         raise Error(f"unknown command {command}")
     cmd(command)
+
+# ----------------------------------------------------------------------
+
+def get_merges():
+    output_dir = Path("merges")
+    output_dir.mkdir(exist_ok=True)
+    from acmacs_whocc import acmacs
+    acmacs.get_recent_merges(output_dir)
 
 # ----------------------------------------------------------------------
 
