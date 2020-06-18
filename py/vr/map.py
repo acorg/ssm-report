c593c3f11a2d6ae02676a754d9cdf6a39b9043df
Eugene Skepner <github@skepner.eu>
2020-06-18 17:35:37 +0200

    vr: make_map

--- py/vr/map.py
+++ py/vr/map.py
@@ -1,11 +1,32 @@
 import subprocess
+from pathlib import Path
 
 # ======================================================================
 
 def make_map(command_name):
     subtype, assay, lab, map_name = command_name.split("-", maxsplit=4)
-    print(f"make_map {command_name} -> {subtype}, {assay}, {lab}, {map_name}")
-    
+    subtype_short = subtype[:2]
+    open_pdf = True
+    interactive = False
+    output_dir = Path("out")
+    output_dir.mkdir(exist_ok=True)
+
+    merge = f"merges/{lab}-{subtype_short}-{assay}.ace"
+    pdf = f"{output_dir}/{subtype}-{assay}-{lab}-{map_name}.pdf"
+    if subtype == "h3":
+        settings = f"-s vr.mapi -s {subtype}.mapi -s {subtype}-{assay}.mapi -s serology.mapi -s vaccines.mapi"
+    else:
+        settings = f"-s vr.mapi -s {subtype}.mapi -s serology.mapi -s vaccines.mapi"
+
+    cmd = f"mapi -a vr:{map_name} {settings} {merge} {pdf}"
+    if open_pdf:
+        cmd += " --open"
+    if interactive:
+        cmd += " -i"
+    #print(f"make_map {command_name} -> {subtype}, {assay}, {lab}, {map_name}")
+    # print(cmd)
+    subprocess.check_call(cmd, shell=True)
+
 # ======================================================================
 ### Local Variables:
 ### eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
--- template-vr/report.py
+++ template-vr/report.py
@@ -3,6 +3,9 @@ def maps():
     return [
         ["h1", "hi", "cdc", "156"],
         ["h1", "hi", "melb", "156"],
+
+        ["bvic", "hi", "cdc", "clade"],
+
         ["~all", "", "", "geographic"],
         ["~all", "", "", "stat"],
         ]
