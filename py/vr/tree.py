6a658a0ba272515554f40d8a2e497f855db81832
Eugene Skepner <github@skepner.eu>
2020-08-28 18:29:17 +0200

    support for making sig pages

--- py/vr/tree.py
+++ py/vr/tree.py
@@ -64,7 +64,39 @@ class sp_maker:
         self.subtype = subtype
         self.lab = lab
         self.assay = assay
+        if self.assay:
+            self.assay_infix = f"-{self.assay}"
+        else:
+            self.assay_infix = ""
         self.sp = sp
+
+    def command_name_for_helm(self):
+        return f"{self.subtype}{self.assay_infix}-{self.lab}-{self.sp}"
+
+    # tal -s ../vr.mapi -s ../h3.mapi -s ../h3-hint.mapi -s ../serology.mapi -s ../vaccines.mapi -s h3.tree.tal -s sp.tal -s h3.sp.tal -s h3.sp.cdc-hint.tal --chart ../merges/cdc-h3-hint.ace h3.tjz h3.cdc-hint.sp.pdf --open
+    # tal -s ../vr.mapi -s ../h1.mapi -s ../h1.mapi -s ../serology.mapi -s ../vaccines.mapi -s h1.tree.tal -s sp.tal -s h1.sp.tal -s h1.sp.cdc.tal --chart ../merges/cdc-h1-hi.ace h1.tjz h1.cdc.sp.pdf --open
+    # tal -s ../vr.mapi -s ../h1.mapi -s ../h1.mapi -s ../serology.mapi -s ../vaccines.mapi -s h1.tree.tal -s sp.tal -s spc.tal -s h1.sp.tal -s h1.sp.cdc.tal --chart ../merges/cdc-h1-hi.ace h1.tjz h1.cdc.spc.pdf --open
+
+    def __call__(self, command_name, interactive, open_pdf=True, output_dir=Path("sp"), *r, **a):
+        source_tjz = output_dir.joinpath(f"{self.subtype}.tjz")
+        tal_settings = output_dir.joinpath(f"{self.subtype}.tree.tal")
+        chart = self.merge()
+        pdf = output_dir.joinpath(f"{self.subtype}.{self.lab}{self.assay_infix}.{self.sp}.pdf")
+        cmd = f"tal -s vr.mapi -s {self.subtype}.mapi"
+        if self.assay:
+            cmd += f" -s {self.subtype}-{self.assay}.mapi"
+        cmd += f" -s serology.mapi -s vaccines.mapi -s sp/{self.subtype}.tree.tal -s sp/sp.tal"
+        if self.sp == "spc":
+            cmd += " -s sp/spc.tal"
+        cmd += f" -s sp/{self.subtype}.sp.tal -s sp/{self.subtype}.sp.{self.lab}{self.assay_infix}.tal"
+        cmd += f" --chart {chart} {source_tjz} {pdf}"
+        if open_pdf:
+            # cmd += " --preview 100.0.834.1000"
+            cmd += " --open"
+        if interactive:
+            cmd += " -i"
+        print(cmd)
+        subprocess.check_call(cmd, shell=True)
 
     def tree(self):
         return f"sp/{self.subtype}.tjz"
@@ -77,13 +109,6 @@ class sp_maker:
 
     def merge_exists(self):
         return Path(self.merge()).exists()
-
-    def command_name_for_helm(self):
-        if self.assay:
-            assay = f"-{self.assay}"
-        else:
-            assay = ""
-        return f"{self.subtype}{assay}-{self.lab}-{self.sp}"
 
 # ----------------------------------------------------------------------
 
